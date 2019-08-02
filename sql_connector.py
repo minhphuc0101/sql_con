54c337e79927b9b97cf92a6de02e6414c2afd0ad
Phuc Tran <minhphuc0101@gmail.com>
2019-08-02 21:39:58 +0700

    connect and fetch data

--- sql_connector.py
+++ sql_connector.py
@@ -1,1 +1,16 @@
-from sqlalchemy import create_engine, Table, 
+from sqlalchemy import *
+metadata = MetaData()
+
+### Connection ###
+engine = create_engine('postgres+psycopg2://srhryast:V1WV12LjQESGf1dALTSbyYOOL7Ifgx8E@john.db.elephantsql.com:5432/srhryast')
+test = Table('test', metadata, autoload=True, autoload_with=engine)
+con = engine.connect()
+
+### List of schema ###
+db_list = inspect(engine).get_schema_names()
+
+query = con.execute('SELECT * FROM minh_phuc_ads.ads_insights LIMIT 1')
+data = query.fetchall()
+con.close()
+
+print (data[0][0])
